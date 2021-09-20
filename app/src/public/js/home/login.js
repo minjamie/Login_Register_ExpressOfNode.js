bd4229b395a696a97df1108c8f19ae7ce9562f24
minjaekim <minjae2246@gmail.com>
2021-09-20 14:54:56 +0900

    DOM을 이용하며 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,15 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+  psw = document.querySelector("#psw"),
+  btn = document.querySelector("button");
+
+btn.addEventListener("click", login);
+
+function login() {
+  const req = {
+    id: id.value,
+    psw: psw.value,
+  };
+  console.log(req);
+}
